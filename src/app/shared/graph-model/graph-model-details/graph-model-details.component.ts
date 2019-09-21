fd31a69919cbfd346a39339f38c91ae536b7df29
Nhuy <nhuy@email.com>
2019-09-20 17:30:35 -0700

    Hide graph model details after creating a new graph model

--- src/app/shared/graph-model/graph-model-details/graph-model-details.component.ts
+++ src/app/shared/graph-model/graph-model-details/graph-model-details.component.ts
@@ -23,12 +23,16 @@ export class GraphModelDetailsComponent implements OnChanges {
   constructor() { }
 
   ngOnChanges(changes: SimpleChanges) {
-    if ('graphModel' in changes && this.graphModel)
-      this.details = [
-        { name: 'Graph name', value: this.graphModel.attributes['Graph name'] },
-        { name: 'A<q, r>', value: this.graphModel.angle },
-        { name: 'S(q, r)', value: this.graphModel.strength },
-      ];
+    if ('graphModel' in changes) {
+      if (this.graphModel)
+        this.details = [
+          { name: 'Graph name', value: this.graphModel.attributes['Graph name'] },
+          { name: 'A<q, r>', value: this.graphModel.angle },
+          { name: 'S(q, r)', value: this.graphModel.strength },
+        ];
+      else
+        this.details = [];
+    }
   }
 
 }
